09e7bca7c9d64dd5031ad0763cdc7f545a915ad2
Bendbds <159724354+Bendbds@users.noreply.github.com>
2024-08-29 15:57:43 +0200

    Delete mentions.js

--- mentions.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.querySelector('.Menu');
-    const headerNav = document.querySelector('.Header');
-
-    menuButton.addEventListener('click', () => {
-        headerNav.classList.toggle('active');
-    });
-});
