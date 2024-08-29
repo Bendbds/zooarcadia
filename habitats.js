7ab7138b6bbf0274fea7f1d5cc25106019d6ade2
Bendbds <159724354+Bendbds@users.noreply.github.com>
2024-08-29 15:54:06 +0200

    Add files via upload

--- habitats.js
+++ habitats.js
@@ -6,35 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
     menuButton.addEventListener('click', () => {
         headerNav.classList.toggle('active');
     });
+});
 
-    const toggleButton = document.getElementById('toggleButton');
-    const form = document.getElementById('habitatForm');
-    const listeCachee = form.querySelector('.liste-cachee');
+document.querySelector('.foret').addEventListener('click', function() {
+    const liste = document.querySelector('.liste-cachee');
+    liste.classList.toggle('liste-visible');
+});
 
-    // Affiche ou cache la liste cachée au clic
-    toggleButton.addEventListener('click', () => {
-        listeCachee.classList.toggle('liste-visible');
-        // Optionnel : Soumettre le formulaire après un court délai pour permettre l'affichage
-        setTimeout(() => {
-            form.submit();
-        }, 1);
-    });
-    
-    document.querySelector('.savane').addEventListener('click', function() {
-        const liste = document.querySelector('.liste-cachee-deux');
-        liste.classList.toggle('liste-visible-deux');
-        enregistrerClic('savane');
-    });
-    
-    document.querySelector('.foret-montagneuse').addEventListener('click', function() {
-        const liste = document.querySelector('.liste-cachee-trois');
-        liste.classList.toggle('liste-visible-trois');
-        enregistrerClic('foret-montagneuse');
-    });
-    
-    document.querySelector('.antarctique').addEventListener('click', function() {
-        const liste = document.querySelector('.liste-cachee-quatre');
-        liste.classList.toggle('liste-visible-quatre');
-        enregistrerClic('antarctique');
-    });
+document.querySelector('.savane').addEventListener('click', function() {
+    const liste = document.querySelector('.liste-cachee-deux');
+    liste.classList.toggle('liste-visible-deux');
+});
+
+document.querySelector('.foret-montagneuse').addEventListener('click', function() {
+    const liste = document.querySelector('.liste-cachee-trois');
+    liste.classList.toggle('liste-visible-trois');
+});
+
+document.querySelector('.antarctique').addEventListener('click', function() {
+    const liste = document.querySelector('.liste-cachee-quatre');
+    liste.classList.toggle('liste-visible-quatre');
 });
